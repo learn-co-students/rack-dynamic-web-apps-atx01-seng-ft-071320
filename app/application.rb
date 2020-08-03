116785264e3c84340a9627272c10b72c711b56f7
timrinkerman <tim.rinkerman98@gmail.com>
2020-08-03 13:31:31 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -6,9 +6,9 @@ class Application
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
     if num_1 == num_2 && num_2 == num_3
-    resp.write "You win"
+    resp.write "You Win"
     else
-      resp.write "You lose, loser"
+      resp.write "You Lose"
     end
     resp.finish
   end
